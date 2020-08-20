341dbc04af8eeb119543873e71242e29d5bb4e90
Carlos Ramirez <carlosdanielrt@gmail.com>
2020-08-19 23:06:18 -0400

    [ADD] library_book: examples

--- library_book/models/library_book.py
+++ library_book/models/library_book.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class LibraryBook(models.Model):
@@ -7,3 +7,10 @@ class LibraryBook(models.Model):
     name = fields.Char("Title", required=True)
     date_release = fields.Date("Release Date")
     author_ids = fields.Many2many("res.partner", string="Authors")
+    show_name = fields.Boolean("Show the name of this book?")
+    limit_users = fields.Boolean("Limit the users display")
+
+    @api.onchange("limit_users")
+    def onchange_limit_users(self):
+        return {"domain": {"author_ids": self.limit_users and [("name", "=", "Carlos")] or False}}
+
