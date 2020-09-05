24cfb8f4153019b8b0c243acbba929895ea6cfa1
FernandoRFR02 <fernandorfr02@gmail.com>
2020-09-05 14:00:37 -0400

    Errors be like

--- library_book/__manifest__.py
+++ library_book/__manifest__.py
@@ -6,11 +6,13 @@
     "license": "",
     "website": "librarybookodoo.com",
     "category": "Inventory",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "depends": ["base", 'decimal_precision'],
     "data": [
         "security/groups.xml",
         "security/ir.model.access.csv",
         "views/library_book.xml",
+        "data/res_partner.xml",
+        "data/demo.xml",
     ],
 }
--- library_book/models/library_book.py
+++ library_book/models/library_book.py
@@ -1,7 +1,9 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 from odoo11.odoo.addons import decimal_precision as dp
 from odoo.fields import Date as fDate
 from datetime import timedelta
+from odoo.exceptions import UserError
+from odoo.tools.translate import _
 
 
 class LibraryBook(models.Model):
@@ -12,12 +14,12 @@ class LibraryBook(models.Model):
     _rec_name = 'short_name'
 
     name = fields.Char("Title", required=True)
+    isbn = fields.Char('ISBN')
     date_release = fields.Date("Release Date")
     author_ids = fields.Many2many('res.partner', string='Authors')
     short_name = fields.Char(string='Short Title', required=True, )
     notes = fields.Text('Internal Notes')
     state = fields.Selection([('draft', 'Not Available'), ('available', 'Available'), ('lost', 'Lost')], 'State')
-
     description = fields.Html(string='Description',
                               sanitize=True,
                               strip_style=False,
@@ -52,10 +54,22 @@ class LibraryBook(models.Model):
         compute_sudo=False,
     )
 
+    ref_doc_id = fields.Reference(
+        selection='_referencable_models',
+        string='Reference Document')
+
+    manager_remarks = fields.Text('Manager Remarks')
+
     _sql_constraints = [
         ('name_unique',
          'UNIQUE (name)',
          'Book title must be unique.')
+    ]
+
+    _sql_constraints = [
+        ('shortname_unique',
+         'UNIQUE (short_name)',
+         'Book Short Name must be unique.')
     ]
 
     @api.constrains('date_release')
@@ -69,31 +83,62 @@ class LibraryBook(models.Model):
     def _compute_age(self):
         today = fDate.from_string(fDate.today())
         for book in self.filtered('date_release'):
-            delta = (today - fDate.from_string(book.date_release))
-            book.age_days = delta.days
+            try:
+                delta = (today - fDate.from_string(book.date_release))
+                book.age_days = delta.days
+            except (IOError, OSError) as exc:
+                message = _('Unable to compute age days') % exc
+                raise UserError(message)
 
     def _inverse_age(self):
         today = fDate.from_string(fDate.context_today(self))
         for book in self.filtered('date_release'):
-            d = today - timedelta(days=book.age_days)
-            book.date_release = fDate.to_string(d)
+            try:
+                d = today - timedelta(days=book.age_days)
+                book.date_release = fDate.to_string(d)
+            except (IOError, OSError) as exc:
+                message = _('Unable to inverse age') % exc
+                raise UserError(message)
 
     def _search_age(self, operator, value):
-        today = fDate.from_string(fDate.context_today(self))
-        value_days = timedelta(days=value)
-        value_date = fDate.to_string(today - value_days)
-        operator_map = {'>': '<', '>=': '<=', '<': '>', '<=': '>=', }
-        new_op = operator_map.get(operator, operator)
-        return [('date_release', new_op, value_date)]
+        try:
+            today = fDate.from_string(fDate.context_today(self))
+            value_days = timedelta(days=value)
+            value_date = fDate.to_string(today - value_days)
+            operator_map = {'>': '<', '>=': '<=', '<': '>', '<=': '>=', }
+            new_op = operator_map.get(operator, operator)
+            return [('date_release', new_op, value_date)]
+        except (IOError, OSError) as exc:
+            message = _('Unable to search by age') % exc
+            raise UserError(message)
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for book in self:
+            authors = book.author_ids.mapped('name')
+            name = '%s (%s)' % (book.name, ', '.join(authors))
+            result.append((book.id, name))
+            return result
+
+    @api.model
+    def _name_search(self, name='', args=None, operator='ilike',
+                     limit=100, name_get_uid=None):
+        args = [] if args is None else args.copy()
+        if not (name == '' and operator == 'ilike'):
+            args += ['|', '|',
+                 ('name', operator, name),
+                 ('isbn', operator, name),
+                 ('author_ids.name', operator, name)
+                 ]
+        return super(LibraryBook, self)._name_search(
+            name='', args=args, operator='ilike',
+            limit=limit, name_get_uid=name_get_uid)
 
     @api.model
     def _referencable_models(self):
         models = self.env['res.request.link'].search([])
         return [(x.object, x.name) for x in models]
-
-    ref_doc_id = fields.Reference(
-        selection='_referencable_models',
-        string='Reference Document')
 
     @api.model
     def is_allowed_transition(self, old_state, new_state):
@@ -113,17 +158,87 @@ class LibraryBook(models.Model):
             else:
                 continue
 
+    @api.model
+    def get_all_library_members(self):
+        library_member_model = self.env['library.member']
+        return library_member_model.search([])
+
+    @api.model
+    def create(self, values):
+        if not self.user_has_groups(
+                'library.group_library_manager'):
+            if 'manager_remarks' in values:
+                raise exceptions.UserError('You are not allowed to modify ''manager_remarks')
+            return super(LibraryBook, self).create(values)
+
+    @api.multi
+    def write(self, values):
+        if not self.user_has_groups('library.group_library_manager'):
+            if 'manager_remarks' in values:
+                raise exceptions.UserError('You are not allowed to modify ''manager_remarks')
+        return super(LibraryBook, self).write(values)
+
+    @api.model
+    def fields_get(self,
+                   allfields=None,
+                   attributes=None):
+        fields = super(LibraryBook, self).fields_get(
+            allfields=allfields,
+            attributes=attributes
+        )
+        if not self.user_has_groups('library.group_library_manager'):
+            if 'manager_remarks' in fields:
+                fields['manager_remarks']['readonly'] = True
+
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     _order = 'name'
+    _name = 'res.partner'
 
-#    published_book_ids = fields.One2many('library.book', 'publisher_id', string='Published Books')
+    name = fields.Char('Name', required=True)
+    email = fields.Char('Email')
+    date = fields.Date('Date')
+    is_company = fields.Boolean(
+        'Is a company',
+        help="Check if the contact is a company, "
+             "otherwise it is a person"
+    )
+    parent_id = fields.Many2one('res.partner', 'Related Company')
+    child_ids = fields.One2many('res.partner', 'parent_id',
+                                'Contacts')
+
+    published_book_ids = fields.One2many('library.book', 'publisher_id', string='Published Books')
     authored_book_ids = fields.Many2many('library.book',
                                          string='Authored Books',
                                          relation='library_book_res_partner_rel')
     count_books = fields.Integer('Number of Authored Books',
                                  compute='_compute_count_books')
+
+    def first_val(self):
+        today_str = fields.Date.context_today(self)
+        val1 = {'name': 'Eric Idle',
+                'email': 'eric.idle@example.com',
+                'date': today_str}
+        val2 = {'name': 'John Cleese',
+                'email': 'john.cleese@example.com',
+                'date': today_str}
+        partner_val = {
+            'name': 'Flying Circus',
+            'email': 'm.python@example.com',
+            'date': today_str,
+            'is_company': True,
+            'child_ids': [(0, 0, val1),
+                          (0, 0, val2),
+                          ]}
+        record = self.env['res.partner'].create(partner_val)
+
+    @api.model
+    def add_contacts(self, partner, contacts):
+        partner.ensure_one()
+        if contacts:
+            partner.date = fields.Date.context_today(self)
+            partner.child_ids |= contacts
 
     @api.depends('authored_book_ids')
     def _compute_count_books(self):
@@ -137,15 +252,62 @@ class ResPartner(models.Model):
                     record.date_release > fields.Date.today()):
                 raise models.ValidationError('Release date must be in the past')
 
+    @api.model
+    def find_partners_and_contacts(self, name):
+        partner = self.env['res.partner']
+        domain = ['|',
+                  '&',
+                  ('is_company', '=', True),
+                  ('name', 'like', name),
+                  '&',
+                  ('is_company', '=', False),
+                  ('parent_id.name', 'like', name)]
+        return partner.search(domain)
+
+    @api.model
+    def partners_with_email(self, partners):
+        def predicate(partner):
+            if partner.email:
+                return True
+            return False
+        return partners.filter(predicate)
+
+    @api.model
+    def get_email_addresses(self, partner):
+        return partner.mapped('child_ids.email')
+
+    @api.model
+    def get_companies(self, partners):
+        return partners.mapped('parent_id')
+
+
 class LibraryMember(models.Model):
     _inherit = {'res.partner': 'partner_id'}
+    _inherit = 'library.member'
     _name = 'library.member'
+    loan_duration = fields.Integer('Loan duration',
+                                   default=15,
+                                   required=True)
+    name = fields.Char('Name', required=True)
+    email = fields.Char('Email')
+    date = fields.Date('Date')
+    is_company = fields.Boolean(
+        'Is a company',
+        help="Check if the contact is a company, "
+             "otherwise it is a person"
+    )
+
     partner_id = fields.Many2one('res.partner',
                                  ondelete='cascade')
     date_start = fields.Date('Member Since')
     date_end = fields.Date('Termination Date')
     member_number = fields.Char()
     date_of_birth = fields.Date('Date of birth')
+
+
+class LibraryBookLoan(models.Model):
+    _inherit = 'library.book.loan'
+    expected_return_date = fields.Date('Due for', required=True)
 
 
 class BaseArchive(models.AbstractModel):
@@ -155,3 +317,31 @@ class BaseArchive(models.AbstractModel):
     def do_archive(self):
         for record in self:
             record.active = not record.active
+
+
+class LibraryLoanWizard(models.TransientModel):
+    _inherit = 'library.load.wizard'
+
+    def _prepare_loan(self, book):
+        values = super(LibraryLoanWizard,self)._prepare_loan(book)
+        loan_duration = self.member_id.loan_duration
+        today_str = fields.Date.context_today(self)
+        today = fields.Date.from_string(today_str)
+        expected = today + timedelta(days=loan_duration)
+        values.update({'expected_return_date':
+        fields.Date.to_string(expected)})
+        return values
+
+    @api.multi
+    def record_loans(self):
+        for wizard in self:
+            books = wizard.book_ids
+            loan = self.env['library.book.loan']
+            for book in wizard.book_ids:
+                values = wizard._prepare_loan(book)
+                loan.create(values)
+
+    @api.multi
+    def _prepare_loan(self, book):
+        return {'member_id': self.member_id.id,
+                'book_id': book.id}
